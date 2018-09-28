975bca8f702fdbb6e4fdba3bfb3922e77096e274
dave <dave@computesoftware.com>
2018-09-28 07:02:34 -0700

    ->>template and as->template

--- src/compute/threadolet.clj
+++ src/compute/threadolet.clj
@@ -30,7 +30,7 @@
       (even? (count bindings)) "an even number of forms in binding vector")
   (let-template* context-fn (partition 2 (destructure bindings)) body nil nil))
 
-(defn- ->template*
+(defn ->template
   [context-fn x forms]
   (let [value-sym (gensym "value")]
     (if forms
@@ -39,10 +39,28 @@
                  (with-meta `(~(first form) ~value-sym ~@(next form)) (meta form))
                  (list form value-sym))]
         `(let [~value-sym ~x]
-           ~(context-fn value-sym (partial ->template* context-fn x' (next forms)))))
+           ~(context-fn value-sym (partial ->template context-fn x' (next forms)))))
       `(let [~value-sym ~x]
          ~(context-fn value-sym (constantly value-sym))))))
 
-(defn ->template
+(defn ->>template
   [context-fn x forms]
-  (->template* context-fn x forms))
+  (let [value-sym (gensym "value")]
+    (if forms
+      (let [form (first forms)
+            x' (if (seq? form)
+                 (with-meta `(~(first form) ~@(next form) ~value-sym) (meta form))
+                 (list form value-sym))]
+        `(let [~value-sym ~x]
+           ~(context-fn value-sym (partial ->>template context-fn x' (next forms)))))
+      `(let [~value-sym ~x]
+         ~(context-fn value-sym (constantly value-sym))))))
+
+(defn as->template
+  [context-fn x name forms]
+  (if forms
+    (let [form (first forms)]
+      `(let [~name ~x]
+         ~(context-fn name (partial as->template context-fn form name (next forms)))))
+    `(let [~name ~x]
+       ~(context-fn name (constantly name)))))
--- test/compute/threadolet_test.clj
+++ test/compute/threadolet_test.clj
@@ -2,15 +2,15 @@
   (:require [clojure.test :refer :all]
             [compute.threadolet :refer :all]))
 
-(defn short-circuit-nil
-  [value-sym recur-fn]
-  `(if (nil? ~value-sym)
+(defn short-circuit
+  [pred value-sym recur-fn]
+  `(if (~pred ~value-sym)
      ~value-sym
      ~(recur-fn)))
 
 (defmacro alet
   [bindings & body]
-  (let-template short-circuit-nil bindings body))
+  (let-template (partial short-circuit nil?) bindings body))
 
 (alet [x (inc 1)
        y (dec x)]
@@ -22,9 +22,11 @@
   (println x y)
   (- x y))
 
+(defn)
+
 (defmacro a->
   [x & forms]
-  (->template short-circuit-nil x forms))
+  (->template (partial short-circuit nil?) x forms))
 
 (a-> 1
      inc
@@ -34,3 +36,30 @@
      inc
      ((constantly nil))
      (- 5))
+
+(defmacro a-as->
+  [x name & forms]
+  (as->template (partial short-circuit nil?) x name forms))
+
+(a-as-> 1 z
+     (inc z)
+     (- 5 z))
+
+(a-as-> 1 z
+     (inc z)
+     ((constantly nil) z)
+     (- 5 z))
+
+(defmacro e->>
+  [x & forms]
+  (->>template (partial short-circuit #(and (seqable? %) (empty? %))) x forms))
+
+(e->> [2 4]
+      (map inc)
+      (filter odd?)
+      empty?)
+
+(e->> [2 4]
+      (map inc)
+      (filter even?)
+      empty?)
